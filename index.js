3489cc96c8438ba62a48e6ec66f45592b1b8ea49
muttons <55284268+muttons@users.noreply.github.com>
2020-02-03 13:24:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+// Register service worker to control making site work offline
+
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/schspa/sw.js').then( registration => {
+      console.log('Service Worker Registered');
+    }, err => {
+      console.log('ServiceWorker registration failed, ${err}');
+
+    });
+  });
+}
+
+
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// enables placeholder text for google cse search bar
+(function() {
+  var cx = '008511990827613828420:oyojv3gjygu';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=008511990827613828420:oyojv3gjygu'+ cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
+
+window.onload = function(){
+document.getElementById('gsc-i-id1').placeholder = 'SEARCH';
+};
---
+++ sw.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'v1.1';
+var urlsToCache = [
+  '/schspa/',
+  '/schspa/index.html',
+  '/schspa/index.js',
+  '/schspa/images/flower-512.png',
+  '/schspa/images/flower-192.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then( cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+
+        }).map(function(cacheName) {
+          return caches.delete(cacheName)
+        })
+      );
+    })
+  );
+});
+
+
+
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+
+    fetch(event.request).catch(() => {
+
+      return caches.match(event.request);
+    })
+  );
+});
