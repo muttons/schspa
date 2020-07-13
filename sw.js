229a59dff7eea2549a5239a58cd2b3b9216c13cb
muttons <55284268+muttons@users.noreply.github.com>
2020-07-13 11:09:01 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v15.5';
-const dynamicCacheName = 'site-dynamic-v15.5';
+const staticCacheName = 'site-static-v15.6';
+const dynamicCacheName = 'site-dynamic-v15.6';
 const assets = [
   '',
   '/index.html',
