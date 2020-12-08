4bd623ffd21c60edf60b13e647ad8b6942cc2cb6
muttons <55284268+muttons@users.noreply.github.com>
2020-12-08 10:10:36 -0500

    added new upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.19';
-const dynamicCacheName = 'site-dynamic-v16.19';
+const staticCacheName = 'site-static-v16.20';
+const dynamicCacheName = 'site-dynamic-v16.20';
 const assets = [
   '',
   '/index.html',
