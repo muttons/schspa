00f52db28d6b08d8f90702fc1e97fddd08a1083e
muttons <55284268+muttons@users.noreply.github.com>
2021-07-21 10:41:34 -0400

    card size changed to500px from 1000px

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.35';
-const dynamicCacheName = 'site-dynamic-v16.35';
+const staticCacheName = 'site-static-v16.36';
+const dynamicCacheName = 'site-dynamic-v16.36';
 const assets = [
   '',
   '/index.html',
