fcbbd3de88057d8fe5e99e3c9ddc281060f74e70
muttons <55284268+muttons@users.noreply.github.com>
2021-08-23 10:34:52 -0400

    parallax rework

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.41';
-const dynamicCacheName = 'site-dynamic-v16.41';
+const staticCacheName = 'site-static-v16.42';
+const dynamicCacheName = 'site-dynamic-v16.42';
 const assets = [
   '',
   '/index.html',
