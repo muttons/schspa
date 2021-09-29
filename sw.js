9d1b90967858a57e48ccbeecdf6db94a36e46a5c
muttons <55284268+muttons@users.noreply.github.com>
2021-09-29 13:05:36 -0400

    flexbox and media quiery for alignment

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.56';
-const dynamicCacheName = 'site-dynamic-v16.56';
+const staticCacheName = 'site-static-v16.57';
+const dynamicCacheName = 'site-dynamic-v16.57';
 const assets = [
   '',
   '/index.html',
