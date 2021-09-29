179d372836612e42650f09fe98e40ec927f4bf6e
muttons <55284268+muttons@users.noreply.github.com>
2021-09-29 10:43:52 -0400

    re-style sponsors section

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.50';
-const dynamicCacheName = 'site-dynamic-v16.50';
+const staticCacheName = 'site-static-v16.51';
+const dynamicCacheName = 'site-dynamic-v16.51';
 const assets = [
   '',
   '/index.html',
