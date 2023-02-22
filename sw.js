d9c94c5cb1802ec152ea3208513a5ae314718665
Mutton <jonathanosterud@gmail.com>
2023-02-22 10:21:06 -0500

    changed main page box.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.96';
-const dynamicCacheName = 'site-dynamic-v16.96';
+const staticCacheName = 'site-static-v16.97';
+const dynamicCacheName = 'site-dynamic-v16.97';
 const assets = [
   '',
   '/index.html',
